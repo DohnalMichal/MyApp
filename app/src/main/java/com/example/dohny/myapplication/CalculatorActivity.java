b2f34aa3ea970b027facb1623ac61cc39d99f973
Dohny <michal.dohnal7@gmail.com>
2017-04-20 10:12:00 +0200

    Přidali jsme vzdálenost #ASW #HAJIjepan

--- app/src/main/java/com/example/dohny/myapplication/CalculatorActivity.java
+++ app/src/main/java/com/example/dohny/myapplication/CalculatorActivity.java
@@ -9,6 +9,5 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        final
     }
 }
