a84b149437a1fe65df1fe258c37c403e8a6044ae
Dohny <michal.dohnal7@gmail.com>
2017-04-23 17:00:03 +0200

    Zprovoznil jsem kalkulačku

--- app/src/main/java/com/example/dohny/myapplication/CalculatorActivity.java
+++ app/src/main/java/com/example/dohny/myapplication/CalculatorActivity.java
@@ -1,26 +1,20 @@
 package com.example.dohny.myapplication;
 
 import android.content.Intent;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.function.DoublePredicate;
 
 public class CalculatorActivity extends AppCompatActivity {
     EditText etVzdalenost, splitMM, splitSS, casMM, casSS;
     Button vzdalenost, split, cas;
-
-   /* int splitVteriny;
-    int splitMinuty;
-    int casMinuty;
-    int casVteriny;
-    int vzdalenost;
-    int split;
-    int cas;   */
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +29,8 @@ public class CalculatorActivity extends AppCompatActivity {
         casSS = (EditText) findViewById(R.id.casSS);
         cas = (Button) findViewById(R.id.bSpocitejCas);
     }
-    //EditText lat_source = (EditText)findViewById(R.id.latValue);
-    //here crash -->    Double sourceLat = Double.parseDouble(lat_source.getText().toString());
 
     public void distanceClicked (View view){
-        /*cas = (casMinuty*60)+(casVteriny);
-        split = (splitMinuty*60)+(splitVteriny);
-        splitVteriny = Integer.parseInt(splitSS.getText().toString());
-        splitMinuty = Integer.parseInt(splitMM.getText().toString());
-        vzdalenost = (cas/split)*500;
-        String vzdalenost1 = new Integer(vzdalenost).toString();
-        ((EditText) findViewById(R.id.etVzdalenost).setText(String.valueOf(vzdalenost1)));*/
         Double splitMinuty = Double.parseDouble(splitMM.getText().toString());
         Double splitVteriny = Double.parseDouble(splitSS.getText().toString());
         Double casVteriny = Double.parseDouble(casSS.getText().toString());
@@ -57,19 +42,44 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
     public void splitClicked (View view) {
-
-
+        Integer distanceEdit = Integer.parseInt(etVzdalenost.getText().toString());
+        Double casVteriny = Double.parseDouble(casSS.getText().toString());
+        Double casMinuty = Double.parseDouble(casMM.getText().toString());
+        double split = 500.0d * (casMinuty + (casVteriny / 60.0d)) / ((double) distanceEdit);
+        String splitMins = getMinutesString((double) ((int) split));
+        String splitSecs = getSecondsString((split % 1.0d) * 60.0d);
+        ((EditText) findViewById(R.id.splitMM)).setText(splitMins);
+        ((EditText) findViewById(R.id.splitSS)).setText(splitSecs);
     }
 
     public void timeClicked (View view) {
-
-
+        Integer distanceEdit = Integer.parseInt(etVzdalenost.getText().toString());
+        Double splitMinuty = Double.parseDouble(splitMM.getText().toString());
+        Double splitVteriny = Double.parseDouble(splitSS.getText().toString());
+        double time = (splitMinuty + (splitVteriny / 60.0d)) * (((double) distanceEdit) / 500.0d);
+        String timeMins = getMinutesString(time);
+        String timeSecs = getSecondsString((time % 1.0d) * 60.0d);
+        ((EditText) findViewById(R.id.casMM)).setText(timeMins);
+        ((EditText) findViewById(R.id.casSS)).setText(timeSecs);
     }
 
-    /*public void calculateDistance(View view) {
-        double split = getDoubleValueFromView(R.id.splitMM) + (getDoubleValueFromView(R.id.splitSS) / 60.0d);
-        ((EditText) findViewById(R.id.etVzdalenost)).setText(String.valueOf(((int) ((getDoubleValueFromView(R.id.casMM) + (getDoubleValueFromView(R.id.casSS) / 60.0d)) / split)) * 500));
-    }  */
+    private String getSecondsString(double secs) {
+        String secString = BuildConfig.FLAVOR;
+        DecimalFormat df = new DecimalFormat("#.#");
+        df.setRoundingMode(RoundingMode.HALF_EVEN);
+        secString = df.format(secs);
+        if (secString.equalsIgnoreCase("0") || secString.equalsIgnoreCase("NaN")) {
+            secString = "00";
+        }
+        if (secs >= 9.9d || secs <= 0.0d) {
+            return secString;
+        }
+        return "0" + secString;
+    }
+
+    private String getMinutesString(double mins) {
+        return String.valueOf((int) Math.floor(mins));
+    }
 
 
 }
